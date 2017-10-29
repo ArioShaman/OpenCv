29f0e2fe74127f444cd231c04860faa7833af391
ArioShaman <ariosha6@gmail.com>
2017-10-30 02:54:14 +0300

    drawing cubes in line

--- cube.py
+++ cube.py
@@ -17,14 +17,10 @@ def draw(img, corners, imgpts):
 
 def drawCube(img, corners, imgpts):
   imgpts = np.int32(imgpts).reshape(-1,2)
-  # draw ground floor in green
-  cv2.drawContours(img, [imgpts[:4]],-1,(246, 171, 96),-3)
-
-  # draw pillars in blue color
+  cv2.drawContours(img, [imgpts[:4]],-1,(0,0,255),1)
   for i,j in zip(range(4),range(4,8)):
     cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),1)
 
-  # draw top layer in red color
   cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),1)
   return img
 
@@ -32,14 +28,11 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 objp = np.zeros((6*7,3), np.float32)
 objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
 
-#this for simple axis
-#axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-
-#this for cube
-axis = np.float32([[0,0,0], [0,1,0], [1,1,0], [1,0,0],
-                   [0,0,-1],[0,1,-1],[1,1,-1],[1,0,-1] ])
 
 for fname in glob.glob('images/*.jpg'):
+  #for cube
+  axis = np.float32([[-1,0,0], [-1,1,0], [0,1,0], [0,0,0],
+                   [-1,0,-1],[-1,1,-1],[0,1,-1],[0,0,-1] ])  
   img = cv2.imread(fname)
   gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
   ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
@@ -54,6 +47,12 @@ for fname in glob.glob('images/*.jpg'):
       # project 3D points to image plane
       imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
       img = drawCube(img,corners,imgpts)
+      
+      for i in xrange(7):
+        axis = axis + [1,0,0] 
+        imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+        img = drawCube(img,corners,imgpts)
+
       cv2.imshow('img',img)
       if cv2.waitKey(1) & 0xFF == ord('q'):
         break
@@ -62,3 +61,4 @@ for fname in glob.glob('images/*.jpg'):
       print 'error'
 
 cv2.destroyAllWindows()
+# x y z
